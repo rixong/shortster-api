b8ef4c4b67910d237460aac15d54c3c741101a79
rixong <rixong@gmail.com>
2021-01-07 13:49:55 -0600

    built route for making shortcode

--- src/index.js
+++ src/index.js
@@ -12,9 +12,26 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  console.log(req.body)
-  const shortCode = `shortCode from ${req.body.customURL}`;
-  res.send(`My Short Code - ${shortCode}`);
+  const { customURL, longURL } = req.body;
+  const regExp = /[a-z0-9]/gi;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  if (customURL) {
+    // validate short URL
+    if (customURL.length < 4) {
+      return res.send('Too short. Must be at least 4 charaters long!');
+    }
+    if (customURL.match(regExp).join('') !== customURL) {
+      return res.send('Only letters and numbers allowed!');
+    }
+    res.send(`Your Short Code is: ${customURL}`);
+  } else {
+    let generatedURL = '';
+    for (let index = 0; index < 6; index += 1) {
+      const idx = Math.floor((Math.random() * characters.length));
+      generatedURL += characters[idx];
+    }
+    res.status(200).send(generatedURL);
+  }
 });
 
 app.listen(port, () => {
