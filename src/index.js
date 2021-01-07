827b1570f80c2814630c121959367ef231995c5a
rixong <rixong@gmail.com>
2021-01-07 11:14:16 -0600

    set up url create route

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors);
+app.use(express.json());
+app.use(cors());
 const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  const shortCode = `shortCode from ${req.body.customURL}`;
+  res.send(`My Short Code - ${shortCode}`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
