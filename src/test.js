99d2629ce62977002ccc1e9e36c7270a2d34a4b0
rixong <rixong@gmail.com>
2021-01-11 17:31:37 -0600

    updated model tests

--- src/test.js
+++ src/test.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-const UserModel = require('./user');
+const URLObject = require('./URLModel');
 
-const userData = { name: 'TekLoon', gender: 'Male', dob: new Date(), loginUsing: 'Facebook' };
+const urlData = { longURL: 'https://movingworlds.org/', shortURL: 'Moving' };
 
-describe('User Model Test', () => {
-  // It's just so easy to connect to the MongoDB Memory Server 
-  // By using mongoose.connect
+describe('URL Model Test', () => {
   beforeAll(async () => {
     await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
       if (err) {
@@ -15,40 +13,36 @@ describe('User Model Test', () => {
     });
   });
 
-  it('create & save user successfully', async () => {
-    const validUser = new UserModel(userData);
-    const savedUser = await validUser.save();
+  it('create & save URL successfully', async () => {
+    const validURL = new URLObject(urlData);
+    const savedURL = await validURL.save();
     // Object Id should be defined when successfully saved to MongoDB.
-    expect(savedUser._id).toBeDefined();
-    expect(savedUser.name).toBe(userData.name);
-    expect(savedUser.gender).toBe(userData.gender);
-    expect(savedUser.dob).toBe(userData.dob);
-    expect(savedUser.loginUsing).toBe(userData.loginUsing);
+    expect(savedURL._id).toBeDefined();
+    expect(savedURL.longURL).toBe(urlData.longURL);
+    expect(savedURL.shortURL).toBe(urlData.shortURL);
+    expect(savedURL.createdAt).toBeDefined();
+    expect(savedURL.lastAccessed).toBeDefined();
+    expect(savedURL.numberOfAccesses).toBeDefined();
   });
 
-  // Test Schema is working!!!
-  // You shouldn't be able to add in any field that isn't defined in the schema
-  it('insert user successfully, but the field does not defined in schema should be undefined', async () => {
-    const userWithInvalidField = new UserModel({ name: 'TekLoon', gender: 'Male', nickname: 'Handsome TekLoon' });
-    const savedUserWithInvalidField = await userWithInvalidField.save();
-    expect(savedUserWithInvalidField._id).toBeDefined();
-    expect(savedUserWithInvalidField.nickkname).toBeUndefined();
+  it('insert url successfully, but the field does not defined in schema should be undefined', async () => {
+    const urlWithInvalidField = new URLObject({ longURL: 'https://movingworlds.org/', shortURL: 'Moving', link: 'Here we go!' });
+    const savedURLWithInvalidField = await urlWithInvalidField.save();
+    expect(savedURLWithInvalidField._id).toBeDefined();
+    expect(savedURLWithInvalidField.link).toBeUndefined();
   });
 
-  // Test Validation is working!!!
-  // It should us told us the errors in on gender field.
   it('create user without required field should failed', async () => {
-    const userWithoutRequiredField = new UserModel({ name: 'TekLoon' });
+    const urlWithoutRequiredField = new URLObject({ longURL: 'www.cnn.com' });
     let err;
     try {
-      const savedUserWithoutRequiredField = await userWithoutRequiredField.save();
-      error = savedUserWithoutRequiredField;
+      const savedURLWithoutRequiredField = await urlWithoutRequiredField.save();
+      error = savedURLWithoutRequiredField;
     } catch (error) {
       err = error
     }
     expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
-    expect(err.errors.gender).toBeDefined();
+    expect(err.errors.shortURL).toBeDefined();
   });
-
 
 })
